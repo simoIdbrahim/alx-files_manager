543470ab9010548eea39528c10512672732a3a03
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 14:46:48 +0000

    add

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ import express from 'express';
 import indexRouter from './routes/index';
 
 const server = express();
+const port = process.env.PORT || 5000;
 
 server.use(indexRouter);
 
-export default server;
+server.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
