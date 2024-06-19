d4141a8d888a47b02bc24651964d04a5ea320d44
simoIdbrahim <134707139+simoIdbrahim@users.noreply.github.com>
2024-06-19 17:25:12 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,4 @@ const router = Router();
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-router.post('/users', UsersController.postNew);
-router.get('/users/me', UsersController.getMe);
-
 export default router;
