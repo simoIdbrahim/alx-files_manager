00d89a161cd820e2dff0af6087d0d240782d8f9c
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 15:21:38 +0000

    add

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,8 @@
 import sha1 from 'sha1';
+import Queue from 'bull/lib/queue';
 import dbClient from '../utils/db';
+
+const userQueue = new Queue('email sending');
 
 export default class UsersController {
   static async postNew(req, res) {
