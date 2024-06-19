3c169cca1cb8044978e35a1a4dac317dd9061531
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 16:05:18 +0000

    add

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ import bodyParser from 'body-parser';
 const server = express();
 const port = process.env.PORT || 5000;
 
-server.use(bodyParser.json());
-
 server.use(indexRouter);
 
 server.listen(port, () => {
