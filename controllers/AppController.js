014dde40866d06b2e4790e98778a9789b9dfd04e
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 13:57:18 +0000

    add

---
+++ controllers/AppController.js
@@ -0,0 +1,25 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+export default class AppController {
+  static getStatus(_, res) {
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
+  }
+
+  static getStats(_, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res
+          .status(200)
+          .json(
+            {
+              users: usersCount,
+              files: filesCount
+            }
+          );
+      });
+  }
+}
