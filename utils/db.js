3f8572f62351270a92a885056c542c86f22e12f7
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 15:42:26 +0000

    add

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,6 @@
+// utils/db.js
 import mongodb from 'mongodb';
 
-// MongoDB collection.
 class DBClient {
   constructor() {
     const host = process.env.DB_HOST || 'localhost';
@@ -9,27 +9,47 @@ class DBClient {
     const dbURL = `mongodb://${host}:${port}/${database}`;
 
     this.client = new mongodb.MongoClient(dbURL, { useUnifiedTopology: true });
-    this.client.connect();
+
+    this.client.connect()
+      .then(() => {
+        console.log('Connected successfully to MongoDB');
+        this.db = this.client.db(database);
+      })
+      .catch((err) => {
+        console.error('Failed to connect to MongoDB', err);
+      });
   }
 
   isAlive() {
-    return this.client.isConnected();
+    return this.client && this.client.topology && this.client.topology.isConnected();
   }
 
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments();
+    if (this.isAlive()) {
+      return this.db.collection('users').countDocuments();
+    }
+    return 0;
   }
 
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments();
+    if (this.isAlive()) {
+      return this.db.collection('files').countDocuments();
+    }
+    return 0;
   }
 
   async usersCollection() {
-    return this.client.db().collection('users');
+    if (this.isAlive()) {
+      return this.db.collection('users');
+    }
+    throw new Error('Not connected to the database');
   }
 
   async filesCollection() {
-    return this.client.db().collection('files');
+    if (this.isAlive()) {
+      return this.db.collection('files');
+    }
+    throw new Error('Not connected to the database');
   }
 }
 
