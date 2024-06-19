393e4081548e7a10962147150823946c19dd6886
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-06-19 15:57:09 +0000

    add

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 // server application entry point
 import express from 'express';
 import indexRouter from './routes/index';
+import bodyParser from 'body-parser';
 
 const server = express();
 const port = process.env.PORT || 5000;
+
+server.use(bodyParser.json());
 
 server.use(indexRouter);
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
